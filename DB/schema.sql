fd2885f9fcd582dc0c0f2d7c1974705365959431
WladSF <wpaivasf@gmail.com>
2019-01-06 22:29:33 -0800

    Created ORM, routes, and handlebars.

--- DB/schema.sql
+++ DB/schema.sql
@@ -3,8 +3,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id INTEGER(15) AUTO_INCREMENT,
-    burger_name VARCHAR(40),
-    devoured BOOLEAN,
+    id INTEGER(15) NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(40) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
--- Models/burger.js
+++ Models/burger.js
@@ -1,12 +1,27 @@
 //Import the ORM to create functions that will interact with the databse
 var orm = require("../Config/orm.js");
 
-//Create the code that will call the ORM functions using 
-//burger specific input for the ORM.
+//Code that will call the ORM functions using burger specific input for the ORM.
 
 var burger = {
+    all: function (cb) {
+        orm.all("burgers", function (res) {
+            cb(res);
+        });
+    },
 
-}
+    create: function (cols, vals, cb) {
+        orm.create("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
 
+    update: function (objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    }
+};
 
+// Export the database functions for the controller (burgers_controller.js).
 module.exports = burger;
