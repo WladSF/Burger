c92126f101a550fbe36d29432a4e8620d30bab28
WladSF <wpaivasf@gmail.com>
2019-01-12 20:34:18 -0800

    Fixing things

--- Controllers/burgers_controller.js
+++ Controllers/burgers_controller.js
@@ -5,42 +5,6 @@ var router = express.Router();
 // Import the model (burger.js) to use its database functions.
 var burger = require("../Models/burger.js");
 
-// Create all our routes and set up logic within those routes where required.
-// router.get("/", function (req, res) {
-//     burger.all(function (data) {
-//         var displayBurgers = {
-//             burgers: data
-//         };
-//         console.log(displayBurgers);
-//         res.render("index", displayBurgers);
-//     });
-// });
-
-// router.post("/api/burgers", function (req, res) {
-//     burger.create([req.body.burger_name], function (result) {
-        // Send back the ID of the new quote
-//         res.json({ id: result.insertId });
-//     });
-// });
-
-// router.put("/api/burgers/:id", function (req, res) {
-//     var condition = "id = " + req.params.id;
-
-//     console.log("condition", condition);
-
-//     burger.update(condition, function (result) {
-//         if (result.changedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
-//             return res.status(404).end();
-//         } else {
-//             console.log("Result: " + result)
-//             res.status(200).end();
-//         }
-//     });
-// });
-
-// Selects * From 'burgers' Table
-//________________________________
 router.get("/", function(req, res) {
     burger.all(function(data) {
       var showBurgers = {
@@ -50,10 +14,6 @@ router.get("/", function(req, res) {
       res.render("index", showBurgers);
     });
 });
-
-
-// Shows 'burgers' Table In JSON Format
-//________________________________
 router.get("/api/burger_data", function(req, res) {
     burger.all(function(data) {
       var showBurgers = {
@@ -64,23 +24,15 @@ router.get("/api/burger_data", function(req, res) {
     });
 });
   
-
-// Posts To 'burgers' Table
-//________________________________
 router.post("/api/burgers/", function(req, res) {
 
     burger.create([
       req.body.burger_name
     ], function(result) {
-      // Send back the ID of the new Burger???
       res.json({ id: result.insertId });
     });
 });
   
-
-
-// Switches 'devoured' In 'burgers' Table To True
-//________________________________
 router.put("/api/burgers/:id", function(req, res) {
     var eatenBurgerID = req.params.id
     var condition = eatenBurgerID;
