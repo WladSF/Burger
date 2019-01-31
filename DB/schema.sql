3bed93c057b8bc3aca3804d177a0e08aa1c66d1e
WladSF <wpaivasf@gmail.com>
2019-01-30 20:30:52 -0800

    Updates

--- DB/schema.sql
+++ DB/schema.sql
@@ -4,6 +4,8 @@ USE burgers_db;
 
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
+    description VARCHAR(255),
+    createdAt TIMESTAMP NOT NULL,
     burger_name VARCHAR(60) NOT NULL,
     devoured BOOLEAN NOT NULL DEFAULT False,
     PRIMARY KEY (id)
